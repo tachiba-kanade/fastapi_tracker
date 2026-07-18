d2c0f4999b6fb9ff2af3249148c9689cf8bceb8c
sudha19 <work.sudhas@gmail.com>
2026-07-18 21:45:37 +0530

    user

--- app/models/user.py
+++ app/models/user.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(
